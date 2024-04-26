54faad71e6113834dd4478ac5a5f340988beee1a
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-26 14:22:09 -0300

    feat: solved 1703b
    first submission attempt

--- codeforces/1703b-icpc_balloons/main.cpp
+++ codeforces/1703b-icpc_balloons/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <map>
 
-int main() {
-  std::string line;
-  while (getline(std::cin, line)) {
-    std::cout << line << "\n";
+int main(int argc, char *argv[]) {
+  int num_teams;
+  std::cin >> num_teams;
+
+  for (int i{1}; i <= num_teams; i++) {
+    int solved_length;
+    std::string solved_problems;
+
+    std::cin >> solved_length;
+    std::cin >> solved_problems;
+
+    std::map<char, bool> map_solved;
+    int sum = 0;
+
+    for (char &c : solved_problems) {
+      if (map_solved[c] == false) {
+        sum += 2;
+        map_solved[c] = true;
+      } else {
+        sum += 1;
+      }
+    }
+
+    std::cout << sum << std::endl;
   }
-
-  return 0;
 }
