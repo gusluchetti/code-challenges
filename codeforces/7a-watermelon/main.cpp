d8f5f513e5b9b1c1d6c5345aa1e5a01ed81acf8c
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-26 22:18:41 -0300

    feat: solved 7a?
    is it just that? lol

---
+++ codeforces/7a-watermelon/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int weight;
+  cin >> weight;
+  if (weight % 2 == 0) {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
+
+  return 0;
+}
