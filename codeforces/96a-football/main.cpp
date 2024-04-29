8dc9c5660899b536f3e6fb94690d76215e08ee13
gusluchetti <gustavo@luchetti.dev>
2024-04-29 11:37:39 -0300

    fix:

--- codeforces/96a-football/main.cpp
+++ codeforces/96a-football/main.cpp
@@ -4,30 +4,39 @@ using namespace std;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+
   string teams;
   bool dangerous = true;
-  cin >> teams;
-  // cout << "\n";
-  // cout << teams;
 
-  if (teams.size() >= 7) {
-    for (int i = 0; i < teams.size() - 7; i++) {
-      char init = teams[i];
-      for (int j = i; j < teams.size(); j++) {
-        if (init != teams[j]) {
-          dangerous = false;
-          break;
+  while (cin >> teams) {
+    if (teams.size() >= 7) {
+      for (int i = 0; i < teams.size() - 7; i++) {
+        int same = 0;
+        for (int j = i; j < teams.size(); j++) {
+          if (teams[i] != teams[j]) {
+            dangerous = false;
+            break;
+          } else {
+            same++;
+          }
+
+          if (same >= 7) {
+            dangerous = true;
+            break;
+          }
         }
-      }
-    }
-  } else {
-    dangerous = false;
-  } 
 
-  if (dangerous) {
-    cout << "YES\n";
-  } else {
-    cout << "NO\n";
+        if (same >= 7) { break; }
+      }
+    } else {
+      dangerous = false;
+    } 
+
+    if (dangerous) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
   }
 
   return 0;
