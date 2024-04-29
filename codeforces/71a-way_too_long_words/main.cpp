c80e6a4d979a137415a287954259f071f0af2a65
gusluchetti <gustavo@luchetti.dev>
2024-04-29 00:22:27 -0300

    feat: submit 1 71a

--- codeforces/71a-way_too_long_words/main.cpp
+++ codeforces/71a-way_too_long_words/main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+  // cout << "\n";
 
-  string temp = "remove this!";
+  int tt;
+  cin >> tt;
+  for(int i = 0; i < tt; i++) {
+    cout << "\n";
+    string word;
+    cin >> word;
+    int size = word.size();
+    if (size > 10) {
+      cout << word[0] << (size - 2) << word[size-1];
+    } else {
+      cout << word;
+    }
+  }
+
   return 0;
 }
