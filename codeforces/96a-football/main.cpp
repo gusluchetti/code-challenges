03681aab63533d70fcd89787fa847c16639a2de9
gusluchetti <gustavo@luchetti.dev>
2024-04-29 10:50:48 -0300

    fix: update loop conditions

--- codeforces/96a-football/main.cpp
+++ codeforces/96a-football/main.cpp
@@ -4,23 +4,24 @@ using namespace std;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-
   string teams;
-  bool dangerous = false;
+  bool dangerous = true;
   cin >> teams;
+  // cout << "\n";
   // cout << teams;
 
   if (teams.size() >= 7) {
-    for (int i = 0; i < teams.size(); i++) {
-      dangerous = true;
+    for (int i = 0; i < teams.size() - 7; i++) {
       char init = teams[i];
-      for (int j = i; j < 7 && j < teams.size(); j++) {
+      for (int j = i; j < teams.size(); j++) {
         if (init != teams[j]) {
           dangerous = false;
           break;
         }
       }
     }
+  } else {
+    dangerous = false;
   } 
 
   if (dangerous) {
